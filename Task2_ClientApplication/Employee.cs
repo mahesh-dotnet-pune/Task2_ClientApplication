d818ca07e22162b29cff1680d102c0d8217d9085
Mahesh.Pathade <Mahesh.Pathade@ENCDAPPUNLT0582>
2025-10-08 16:57:33 +0530

    Add project files.

---
+++ Task2_ClientApplication/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_ClientApplication
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        public string Department { get; set; }
+
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Mobile number must be 10 digits.")]
+        public string MobileNo { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
---
+++ Task2_ClientApplication/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace Task2_ClientApplication
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Fetching products from the Web API...");
+
+            var handler = new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback =
+                    (message, cert, chain, errors) => true
+            };
+
+            HttpClient client = new HttpClient(handler);
+
+            try
+            {
+                // Replace with your actual API endpoint
+                var response = await client.GetAsync("http://localhost:5014/api/employee");
+
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("Response from API:");
+                    Console.WriteLine(content);
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception from API: {ex.Message}");
+            }
+        }
+    }
+}
